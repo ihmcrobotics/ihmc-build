33298707dc117c4819b7a4eb196768f52e2f3080
Doug Stephen <dstephen@ihmc.us>
2016-02-09 12:13:40 -0600

    NONE: Add clojars to default repositories

--- groovySrc/us/ihmc/gradle/IHMCBuildExtension.groovy
+++ groovySrc/us/ihmc/gradle/IHMCBuildExtension.groovy
@@ -35,6 +35,10 @@ class IHMCBuildExtension {
             jcenter()
 
             mavenCentral()
+
+            maven{
+                url "http://clojars.org/repo/"
+            }
         }
     }
 
@@ -89,13 +93,7 @@ class IHMCBuildExtension {
      * @return The fully qualified Gradle path to the project as given by {@link Project#getPath()}
      */
     def String getProjectDependencyGradlePath(String projectName) {
-        def project = getProjectDependency(projectName)
-
-        if (project != null) {
-            return project.path
-        }
-
-        return null
+        return getProjectDependency(projectName).path
     }
 
     def void configureForIHMCOpenSourceBintrayPublish(boolean isDryRun, String mavenPublicationName, String bintrayRepoName, List<String> packageLabels) {
