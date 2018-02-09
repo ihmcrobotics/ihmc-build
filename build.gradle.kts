b16acbaf82a74082a071bb9f01060eedd8bc0afa
Duncan Calvert <dcalvert@ihmc.us>
2018-02-09 11:24:46 -0600

    :arrow_up: ihmc-ci 0.17.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,7 +53,7 @@ repositories {
 dependencies {
    compile("ca.cutterslade.gradle:gradle-dependency-analyze:1.2.0")
    compile("gradle.plugin.com.dorongold.plugins:task-tree:1.3")
-   compile("us.ihmc:ihmc-ci-plugin:0.17.4")
+   compile("us.ihmc:ihmc-ci-plugin:0.17.6")
    compile("org.jfrog.artifactory.client:artifactory-java-client-services:2.5.1")
    compile(gradleKotlinDsl())
 }
