6f1b06314db521fa3a1b517d59b12f85b42880b7
Duncan Calvert <dcalvert@ihmc.us>
2018-05-14 18:42:28 -0500

    Add testSuiteGenerator test.

---
+++ src/test/builds/generateTestSuitesTest/src/test/java/us/ihmc/generatedTestSuites/YourProjectAFastTestSuite.java
@@ -0,0 +1,24 @@
+package us.ihmc.generatedTestSuites;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite.SuiteClasses;
+
+import us.ihmc.continuousIntegration.ContinuousIntegrationSuite;
+import us.ihmc.continuousIntegration.ContinuousIntegrationSuite.ContinuousIntegrationSuiteCategory;
+import us.ihmc.continuousIntegration.IntegrationCategory;
+
+/** WARNING: AUTO-GENERATED FILE. DO NOT MAKE MANUAL CHANGES TO THIS FILE. **/
+@RunWith(ContinuousIntegrationSuite.class)
+@ContinuousIntegrationSuiteCategory(IntegrationCategory.FAST)
+@SuiteClasses
+({
+   us.ihmc.BasicTestApplicationTest.class
+})
+
+public class YourProjectAFastTestSuite
+{
+   public static void main(String[] args)
+   {
+
+   }
+}
