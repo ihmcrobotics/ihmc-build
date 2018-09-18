758d136268ef205e711349417b2fa2e728db6a18
Duncan Calvert <dcalvert@ihmc.us>
2018-09-18 14:10:34 -0500

    Add Junit 5 categories tests.

---
+++ src/junitfive-test/builds/categories/src/test/java/us/ihmc/ci/CategorizedTests1.java
@@ -0,0 +1,46 @@
+package us.ihmc.ci;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
+import org.junit.jupiter.api.parallel.ResourceAccessMode;
+import org.junit.jupiter.api.parallel.ResourceLock;
+
+public class CategorizedTests1
+{
+   @Tag("fast")
+   @Test
+   public void fastTest() throws InterruptedException
+   {
+   }
+
+   @Test
+   public void untaggedTest()
+   {
+
+   }
+
+   @Tag("failing")
+   @Test
+   public void failingTest() throws InterruptedException
+   {
+      Assertions.fail();
+   }
+
+   @ResourceLock(value = "File.txt", mode = ResourceAccessMode.READ_WRITE)
+   @Execution(ExecutionMode.SAME_THREAD)
+   @Tag("slow")
+   @Test
+   public void slowTest() throws InterruptedException
+   {
+   }
+
+   @Tag("fast")
+   @Tag("needs-gpu")
+   @Test
+   public void gpuTest() throws InterruptedException
+   {
+   }
+}
--- src/junitfive-test/java/us/ihmc/ci/GradleSubBuildTools.java
+++ src/junitfive-test/java/us/ihmc/ci/GradleSubBuildTools.java
@@ -3,38 +3,41 @@ package us.ihmc.ci;
 import org.apache.commons.io.input.TeeInputStream;
 import org.apache.commons.io.output.ByteArrayOutputStream;
 import org.apache.commons.io.output.TeeOutputStream;
+import org.apache.commons.lang3.SystemUtils;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
+import java.nio.charset.Charset;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class GradleSubBuildTools
 {
    public static String buildsDir = "builds/";
-   public static String gradleExe = "/usr/bin/gradle --stacktrace --info";
+   public static String gradleCommand = SystemUtils.IS_OS_WINDOWS ? "gradlew.bat" : "gradlew";
+   public static String gradleExe = Paths.get(buildsDir + gradleCommand).toAbsolutePath().toString();
 
    public static String runCommand(String command, Path workingDir)
    {
       try
       {
          String[] parts = command.split("\\s");
-         Process processBuilder = new ProcessBuilder(parts).directory(workingDir.toFile())
-                                                           .redirectOutput(ProcessBuilder.Redirect.PIPE)
-                                                           .redirectError(ProcessBuilder.Redirect.PIPE)
-                                                           .start();
+         Process proc = new ProcessBuilder(parts)
+               .directory(workingDir.toFile())
+               .redirectOutput(ProcessBuilder.Redirect.PIPE)
+               .redirectError(ProcessBuilder.Redirect.PIPE)
+               .start();
 
-         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-         TeeOutputStream teeOutputStream = new TeeOutputStream(System.out, byteArrayOutputStream);
-         TeeInputStream teeSysOut = new TeeInputStream(processBuilder.getInputStream(), teeOutputStream);
-         TeeInputStream teeSysErr = new TeeInputStream(processBuilder.getErrorStream(), teeOutputStream);
-         while (processBuilder.isAlive())
+         ByteArrayOutputStream baos = new ByteArrayOutputStream();
+         TeeOutputStream teeOutputStream = new TeeOutputStream(System.out, baos);
+         TeeInputStream teeSysOut = new TeeInputStream(proc.getInputStream(), teeOutputStream);
+         TeeInputStream teeSysErr = new TeeInputStream(proc.getErrorStream(), teeOutputStream);
+         while (proc.isAlive())
          {
             readAllFromTee(teeSysOut, teeSysErr);
          }
          readAllFromTee(teeSysOut, teeSysErr);
 
-         String output = byteArrayOutputStream.toString(StandardCharsets.UTF_8);
+         String output = baos.toString(Charset.forName("UTF-8"));
          return output;
       }
       catch (IOException | InterruptedException e)
@@ -46,21 +49,16 @@ public class GradleSubBuildTools
 
    private static void readAllFromTee(TeeInputStream teeSysOut, TeeInputStream teeSysErr) throws IOException, InterruptedException
    {
-      while (teeSysOut.read() > -1 || teeSysErr.read() > -1)
-         ;
+      while (teeSysOut.read() > -1 || teeSysErr.read() > -1);
       Thread.sleep(200);
    }
 
    public static String runGradleTask(String command, String project)
    {
-      String commandString = gradleExe;
-      if (command != null && !command.isEmpty())
-      {
-         commandString += " " + command;
-      }
-
-      System.out.println("Running " + commandString + " in " + Paths.get(buildsDir + project).toAbsolutePath());
-
-      return runCommand(gradleExe + " " + command, Paths.get(buildsDir + project).toAbsolutePath());
+      System.out.println("Running " + gradleExe + " in " + Paths.get(buildsDir + project).toAbsolutePath());
+      if (command == null || command.isEmpty())
+         return runCommand(gradleExe, Paths.get("builds/" + project).toAbsolutePath());
+      else
+         return runCommand(gradleExe + " " + command, Paths.get(buildsDir + project).toAbsolutePath());
    }
 }
