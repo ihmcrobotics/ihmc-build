14881e448cd5dedad4adb47b1e823376310d0f96
Duncan Calvert <dcalvert@ihmc.us>
2018-09-21 12:21:04 -0500

    Start on testing parallel execution.

--- src/junitfive-test/java/us/ihmc/ci/ParallelExecutionTest.java
+++ src/junitfive-test/java/us/ihmc/ci/ParallelExecutionTest.java
@@ -3,60 +3,39 @@ package us.ihmc.ci;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import us.ihmc.commons.nio.FileTools;
-import us.ihmc.log.LogTools;
 
 import java.io.UnsupportedEncodingException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Paths;
 
-/**
- * Must be run from ihmc-ci/src/junitfive-test root directory.
- */
 public class ParallelExecutionTest
 {
    @Test
    public void testAllTestsRun() throws UnsupportedEncodingException
    {
       String projectName = "categories";
-      GradleSubBuildTools.runGradleTask("-v", projectName);
       String cleanOutput = GradleSubBuildTools.runGradleTask("clean", projectName);
       Assertions.assertTrue(cleanOutput.contains("BUILD SUCCESSFUL"));
-      String output = GradleSubBuildTools.runGradleTask("test --info -Pcategory=all", projectName);
+      String output = GradleSubBuildTools.runGradleTask("test", projectName);
       Assertions.assertTrue(output.contains("BUILD FAILED"));
 
-      LogTools.info("Working dir: " + Paths.get(".").toAbsolutePath());
-      String results = new String(FileTools.readAllBytes(Paths.get("builds/categories/src/test/build/reports/tests/test/index.html"),
-                                                         Assertions::fail),
-                                  StandardCharsets.UTF_8);
-      System.out.println(results);
+      String results = new String(FileTools.readAllBytes(Paths.get("builds/categories/src/test/build/reports/tests/test/index.html"), e -> Assertions.fail(e)), "UTF-8");
+
       // Asserts 11 tests pass, 1 test fails, 0 tests ignored
-      Assertions.assertTrue(results.contains("<a href=\"packages/us.ihmc.ci.html\">us.ihmc.ci</a>" + System.lineSeparator() +
-                                                   "</td>" + System.lineSeparator() +
-                                                   "<td>9</td>" + System.lineSeparator() +
-                                                   "<td>1</td>" + System.lineSeparator() +
-                                                   "<td>0</td>"));
+      Assertions.assertTrue(results.contains("<a href=\"packages/us.ihmc.ci.html\">us.ihmc.ci</a>\r\n</td>\r\n<td>11</td>\r\n<td>1</td>\r\n<td>0</td>"));
    }
 
    @Test
    public void testFastTestsRun() throws UnsupportedEncodingException
    {
       String projectName = "categories";
-      GradleSubBuildTools.runGradleTask("-v", projectName);
       String cleanOutput = GradleSubBuildTools.runGradleTask("clean", projectName);
       Assertions.assertTrue(cleanOutput.contains("BUILD SUCCESSFUL"));
-      String output = GradleSubBuildTools.runGradleTask("test --info -Pcategory=fast", projectName);
+      String output = GradleSubBuildTools.runGradleTask("test -PincludeTags=fast", projectName);
       Assertions.assertTrue(output.contains("BUILD SUCCESSFUL"));
 
-      LogTools.info("Working dir: " + Paths.get(".").toAbsolutePath());
-      String results = new String(FileTools.readAllBytes(Paths.get("builds/categories/src/test/build/reports/tests/test/index.html"),
-                                                         Assertions::fail),
-                                  StandardCharsets.UTF_8);
-      System.out.println(results);
+      String results = new String(FileTools.readAllBytes(Paths.get("builds/categories/src/test/build/reports/tests/test/index.html"), e -> Assertions.fail(e)), "UTF-8");
+
       // Asserts 5 tests pass, 0 test fails, 0 tests ignored
-      Assertions.assertTrue(results.contains("<a href=\"packages/us.ihmc.ci.html\">us.ihmc.ci</a>" + System.lineSeparator() +
-                                                   "</td>" + System.lineSeparator() +
-                                                   "<td>4</td>" + System.lineSeparator() +
-                                                   "<td>0</td>" + System.lineSeparator() +
-                                                   "<td>0</td>"));
+      Assertions.assertTrue(results.contains("<a href=\"packages/us.ihmc.ci.html\">us.ihmc.ci</a>\r\n</td>\r\n<td>5</td>\r\n<td>0</td>\r\n<td>0</td>"));
    }
 }
