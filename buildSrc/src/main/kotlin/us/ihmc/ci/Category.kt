d930d9dacfaa9858d8c1cc3bacd48fc3c964b0b2
Duncan Calvert <dcalvert@ihmc.us>
2018-10-17 14:37:51 -0500

    Setup default categories.

--- buildSrc/src/main/kotlin/us/ihmc/ci/Category.kt
+++ buildSrc/src/main/kotlin/us/ihmc/ci/Category.kt
@@ -1,13 +1,43 @@
 package us.ihmc.ci
 
-class Category (val name: String)
+import org.gradle.api.GradleException
+import org.gradle.api.Project
+
+class Category (val name: String, private val project: Project)
 {
    var classesPerJVM = 1
    var maxJVMs = 2
    var maxParallelTests = 4
-   var excludeTags = arrayListOf("all")
-   var includeTags = arrayListOf<String>()
+   val excludeTags = arrayListOf("all")
+   val includeTags = arrayListOf<String>()
    val jvmArgs = arrayListOf<String>()
    var minHeapSizeGB = 1
    var maxHeapSizeGB = 4
+
+   fun getScsDefaultJVMArgs(): Array<String>
+   {
+      return arrayOf("-Dcreate.scs.gui=false",
+                     "-Dshow.scs.windows=false",
+                     "-Dshow.scs.yographics=false",
+                     "-Djava.awt.headless=true",
+                     "-Dcreate.videos=false",
+                     "-Dopenh264.license=accept",
+                     "-Ddisable.joint.subsystem.publisher=true",
+                     "-Dscs.dataBuffer.size=8142"
+      )
+   }
+
+   fun getAllocationAgentJVMArg(): String
+   {
+      project.project(project.name + "-test").configurations.getByName("compile").files.forEach {
+         if (it.name.contains("java-allocation-instrumenter"))
+         {
+            val allocationJVMArg = "-javaagent:" + it.getAbsolutePath()
+            println("[ihmc-ci] Found allocation JVM arg: " + allocationJVMArg)
+            return allocationJVMArg
+         }
+      }
+
+      throw GradleException("[ihmc-ci] Cannot find `java-allocation-instrumenter` on test classpath. Please add it to your test dependencies!")
+   }
 }
