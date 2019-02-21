837c77898669f4f9e7a9537331458a85143f8f65
Duncan Calvert <dcalvert@ihmc.us>
2019-02-20 21:34:43 -0600

    Add a test build for classpath testing. Update wrapper to Gradle 5.2.1

---
+++ tests/multi-project-composite-source-sets/sub-project-one/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+   id("us.ihmc.ihmc-build") version "0.15.6"
+}
+
+ihmc {
+   group = "us.ihmc"
+   version = "0.1.0"
+   vcsUrl = "https://your.vcs/url"
+   openSource = false
+
+   configureDependencyResolution()
+   configurePublications()
+}
+
+println("hello")
+
+dependencies {
+   compile("org.apache.commons:commons-lang3:3.8.1")
+
+   testCompile("org.junit.jupiter:junit-jupiter-api:5.4.0")
+}
+
+mainDependencies {
+
+}
+
+testDependencies {
+
+}
---
+++ tests/multi-project-composite-source-sets/sub-project-two/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+   id("us.ihmc.ihmc-build") version "0.15.6"
+}
+
+ihmc {
+   group = "us.ihmc"
+   version = "0.1.0"
+   vcsUrl = "https://your.vcs/url"
+   openSource = false
+
+   configureDependencyResolution()
+   configurePublications()
+}
+
+println("hello")
+
+dependencies {
+   compile("commons-io:commons-io:2.6")
+
+   testCompile("junit:junit:4.12")
+}
+
+mainDependencies {
+}
+
+testDependencies {
+
+}
