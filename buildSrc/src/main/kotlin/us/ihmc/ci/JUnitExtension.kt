131de27b347d9f6fe91e70861a2cb8734d3415ac
Duncan Calvert <dcalvert@ihmc.us>
2019-09-25 10:36:13 -0500

    :bookmark: 5.3 Add extension for accessing JUnit 5 versions

--- buildSrc/src/main/kotlin/us/ihmc/ci/JUnitExtension.kt
+++ buildSrc/src/main/kotlin/us/ihmc/ci/JUnitExtension.kt
@@ -1,10 +1,3 @@
 package us.ihmc.ci
 
-class JUnitExtension(val jupiterVersion: String,
-                     val platformVersion: String)
-{
-   fun jupiterApi(): String = "org.junit.jupiter:junit-jupiter-api:$jupiterVersion"
-   fun jupiterEngine(): String = "org.junit.jupiter:junit-jupiter-engine:$jupiterVersion"
-   fun platformCommons(): String = "org.junit.platform:junit-platform-commons:$platformVersion"
-   fun platformLauncher(): String = "org.junit.platform:junit-platform-launcher:$platformVersion"
-}
+class JUnitExtension(val jupiter: String, val platform: String)
