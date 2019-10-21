e63beb51b56579d2e9ea633593e44fc32783f65c
Duncan Calvert <dcalvert@ihmc.us>
2019-10-21 09:57:55 -0500

    :bookmark: 0.19.7 Fix publishing with credentials

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "us.ihmc"
-version = "0.19.6"
+version = "0.19.7"
 
 repositories {
    jcenter()
--- src/main/kotlin/us/ihmc/build/IHMCPublishUrl.kt
+++ src/main/kotlin/us/ihmc/build/IHMCPublishUrl.kt
@@ -4,6 +4,6 @@ class IHMCPublishUrl(val url: String, val username: String, val password: String
 {
    fun hasCredentials(): Boolean
    {
-      return username.isEmpty() || password.isEmpty()
+      return !(username.isEmpty() || password.isEmpty())
    }
 }
