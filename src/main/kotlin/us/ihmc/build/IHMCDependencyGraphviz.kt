1e34fb5c37a70cf055506ae62f8236b9241a697f
Duncan Calvert <dcalvert@ihmc.us>
2020-07-05 15:45:49 -0500

    Get some dependencies printing.

--- src/main/kotlin/us/ihmc/build/IHMCDependencyGraphviz.kt
+++ src/main/kotlin/us/ihmc/build/IHMCDependencyGraphviz.kt
@@ -1,7 +1,11 @@
 package us.ihmc.build
 
 import org.gradle.api.Project
+import org.gradle.api.plugins.JavaPluginConvention
 
+/**
+ * TODO: Support graphing sources and jars
+ */
 class IHMCDependencyGraphviz(val project: Project)
 {
    init
@@ -9,7 +13,28 @@ class IHMCDependencyGraphviz(val project: Project)
       project.tasks.create("graphDependencies")
       {
          doLast {
+            for (includedBuild in project.gradle.includedBuilds)
+            {
+               LogTools.warn("Included build: " + includedBuild.name)
+            }
 
+            for (configurationName in project.configurations.names)
+            {
+               LogTools.warn("configurationName: " + configurationName)
+            }
+
+            for (sourceSet in project.convention.getPlugin(JavaPluginConvention::class.java).sourceSets)
+            {
+               for (fileSystemLocation in sourceSet.runtimeClasspath.elements.get())
+               {
+                  LogTools.warn("Dependency " + fileSystemLocation.toString())
+               }
+            }
+
+//            for (allDependency in project.configurations.getByName().allDependencies)
+//            {
+//               LogTools.warn("Dependency " + allDependency.name)
+//            }
          }
       }
    }
