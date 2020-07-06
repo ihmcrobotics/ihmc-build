50c176c6cf12e6a895f20d97a5eb6e19bb934fed
Duncan Calvert <dcalvert@ihmc.us>
2020-07-06 14:49:47 -0500

    Transitive dependency access working

--- src/main/kotlin/us/ihmc/build/IHMCDependencyGraphviz.kt
+++ src/main/kotlin/us/ihmc/build/IHMCDependencyGraphviz.kt
@@ -1,13 +1,20 @@
 package us.ihmc.build
 
 import org.gradle.api.Project
+import org.gradle.api.artifacts.Configuration
+import org.gradle.api.artifacts.ResolvedDependency
 import org.gradle.api.plugins.JavaPluginConvention
+import org.gradle.kotlin.dsl.get
+import org.gradle.kotlin.dsl.withType
 
 /**
  * TODO: Support graphing sources and jars
  */
 class IHMCDependencyGraphviz(val project: Project)
 {
+   data class IHMCDependency(val group: String, val name: String, val version: String)
+   val dependencies = sortedSetOf<String>()
+
    init
    {
       project.tasks.create("graphDependencies")
@@ -23,18 +30,64 @@ class IHMCDependencyGraphviz(val project: Project)
                LogTools.warn("configurationName: " + configurationName)
             }
 
-            for (sourceSet in project.convention.getPlugin(JavaPluginConvention::class.java).sourceSets)
+//            for (configuration in project.configurations)
+//            {
+//                  LogTools.warn("Configuration " + configuration.name)
+//
+//            }
+
+            for (firstLevelModuleDependency in project.configurations["runtimeClasspath"].resolvedConfiguration.firstLevelModuleDependencies)
             {
-               for (fileSystemLocation in sourceSet.runtimeClasspath.elements.get())
-               {
-                  LogTools.warn("Dependency " + fileSystemLocation.toString())
-               }
+               recursiveHandleDependency(firstLevelModuleDependency)
+
             }
+
+//            LogTools.warn("Class: " + project.configurations["runtimeClasspath"]::class.supertypes)
+//            for (dependency in project.configurations["runtimeClasspath"].allDependencies)
+//            {
+//               LogTools.warn("Dependency " + dependency)
+//            }
+//            for (file in project.configurations["runtimeClasspath"].resolve())
+//            {
+//               LogTools.warn("Resolved " + file)
+//
+//            }
+//            for (artifact in project.configurations["runtimeClasspath"].outgoing.artifacts)
+//            {
+//               LogTools.warn("Outgoing artifact " + artifact)
+//            }
+
+//            for (sourceSet in project.convention.getPlugin(JavaPluginConvention::class.java).sourceSets)
+//            {
+//               for (fileSystemLocation in sourceSet.runtimeClasspath.elements.get())
+//               {
+//                  LogTools.warn("Dependency " + fileSystemLocation.toString())
+//               }
+//            }
 
 //            for (allDependency in project.configurations.getByName().allDependencies)
 //            {
 //               LogTools.warn("Dependency " + allDependency.name)
 //            }
+         }
+      }
+   }
+
+   private fun recursiveHandleDependency(firstLevelModuleDependency: ResolvedDependency)
+   {
+      LogTools.warn("First level module dependency: " + firstLevelModuleDependency)
+      firstLevelModuleDependency.run {
+//         dependencies.add(IHMCDependency(moduleGroup, moduleName, moduleVersion))
+         dependencies.add("$moduleGroup:$moduleName:$moduleVersion")
+      }
+
+      for (child in firstLevelModuleDependency.children)
+      {
+//         if (!dependencies.contains(IHMCDependency(child.moduleGroup, child.moduleName, child.moduleVersion)))
+         if (!dependencies.contains("${child.moduleGroup}:${child.moduleName}:${child.moduleVersion}"))
+         {
+            LogTools.warn("Child: " + child)
+            recursiveHandleDependency(child)
          }
       }
    }
