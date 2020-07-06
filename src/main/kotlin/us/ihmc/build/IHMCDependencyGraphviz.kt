6fc3b1cdd612d85b96e8ddfeba1c038435479380
Duncan Calvert <dcalvert@ihmc.us>
2020-07-06 15:43:58 -0500

    Progress on GraphViz but it's running out of memory.

--- src/main/kotlin/us/ihmc/build/IHMCDependencyGraphviz.kt
+++ src/main/kotlin/us/ihmc/build/IHMCDependencyGraphviz.kt
@@ -1,93 +1,97 @@
 package us.ihmc.build
 
+import guru.nidi.graphviz.engine.Format
+import guru.nidi.graphviz.model.Factory
+import guru.nidi.graphviz.model.MutableGraph
+import guru.nidi.graphviz.model.MutableNode
+import guru.nidi.graphviz.toGraphviz
 import org.gradle.api.Project
-import org.gradle.api.artifacts.Configuration
 import org.gradle.api.artifacts.ResolvedDependency
-import org.gradle.api.plugins.JavaPluginConvention
 import org.gradle.kotlin.dsl.get
-import org.gradle.kotlin.dsl.withType
+import java.io.File
+import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
+import kotlin.collections.HashMap
 
 /**
  * TODO: Support graphing sources and jars
  */
 class IHMCDependencyGraphviz(val project: Project)
 {
-   data class IHMCDependency(val group: String, val name: String, val version: String)
-   val dependencies = sortedSetOf<String>()
+
+   val previouslyExplored = sortedSetOf<String>()
+
+   lateinit var graph: MutableGraph
+   lateinit var nodeMap: HashMap<String, MutableNode>
 
    init
    {
       project.tasks.create("graphDependencies")
       {
          doLast {
-            for (includedBuild in project.gradle.includedBuilds)
-            {
-               LogTools.warn("Included build: " + includedBuild.name)
-            }
 
-            for (configurationName in project.configurations.names)
-            {
-               LogTools.warn("configurationName: " + configurationName)
-            }
+            graph = Factory.mutGraph("graph name?").setDirected(true)
+            nodeMap = HashMap()
 
-//            for (configuration in project.configurations)
-//            {
-//                  LogTools.warn("Configuration " + configuration.name)
-//
-//            }
+            val projectName = "${project.group}:${project.name}:${project.version}"
+            nodeMap[projectName] = Factory.mutNode(projectName)
 
             for (firstLevelModuleDependency in project.configurations["runtimeClasspath"].resolvedConfiguration.firstLevelModuleDependencies)
             {
-               recursiveHandleDependency(firstLevelModuleDependency)
+               val id = "${firstLevelModuleDependency.moduleGroup}:${firstLevelModuleDependency.moduleName}:${firstLevelModuleDependency.moduleVersion}"
 
+               if (!nodeMap.containsKey(id))
+               {
+                  nodeMap[id] = Factory.mutNode(id)
+               }
+
+               nodeMap[projectName]!!.addLink(nodeMap[id])
+
+               if (!previouslyExplored.contains(id))
+               {
+                  previouslyExplored.add(id)
+                  recursiveHandleDependency(firstLevelModuleDependency, id)
+               }
             }
 
-//            LogTools.warn("Class: " + project.configurations["runtimeClasspath"]::class.supertypes)
-//            for (dependency in project.configurations["runtimeClasspath"].allDependencies)
-//            {
-//               LogTools.warn("Dependency " + dependency)
-//            }
-//            for (file in project.configurations["runtimeClasspath"].resolve())
-//            {
-//               LogTools.warn("Resolved " + file)
-//
-//            }
-//            for (artifact in project.configurations["runtimeClasspath"].outgoing.artifacts)
-//            {
-//               LogTools.warn("Outgoing artifact " + artifact)
-//            }
+            nodeMap.values.forEach { graph.add(it) }
 
-//            for (sourceSet in project.convention.getPlugin(JavaPluginConvention::class.java).sourceSets)
-//            {
-//               for (fileSystemLocation in sourceSet.runtimeClasspath.elements.get())
-//               {
-//                  LogTools.warn("Dependency " + fileSystemLocation.toString())
-//               }
-//            }
+            val dateFormat = SimpleDateFormat("yyyyMMdd_HHmmssSSS");
+            val calendar = Calendar.getInstance();
+            val timestamp = dateFormat.format(calendar.getTime());
+            val filePathName = System.getProperty("user.home") + "/.ihmc/logs/" + timestamp + "_" + "_DependencyGraph.png";
+//            val filePathName = System.getProperty("user.home") + "/" + LocalDateTime.now().format(dateFormatter) + "_DependencyGraph.png"
 
-//            for (allDependency in project.configurations.getByName().allDependencies)
-//            {
-//               LogTools.warn("Dependency " + allDependency.name)
-//            }
+            graph.toGraphviz().render(Format.PNG).toFile(File(filePathName))
+
+            LogTools.quiet("Dependency graph saved to " + filePathName)
          }
       }
    }
 
-   private fun recursiveHandleDependency(firstLevelModuleDependency: ResolvedDependency)
+   private fun recursiveHandleDependency(firstLevelModuleDependency: ResolvedDependency, id: String)
    {
-      LogTools.warn("First level module dependency: " + firstLevelModuleDependency)
-      firstLevelModuleDependency.run {
-//         dependencies.add(IHMCDependency(moduleGroup, moduleName, moduleVersion))
-         dependencies.add("$moduleGroup:$moduleName:$moduleVersion")
-      }
+//      LogTools.warn("nodeMap size: " + nodeMap.)
+//      LogTools.warn("nodeMap size: " + graph.tot)
+
 
       for (child in firstLevelModuleDependency.children)
       {
-//         if (!dependencies.contains(IHMCDependency(child.moduleGroup, child.moduleName, child.moduleVersion)))
-         if (!dependencies.contains("${child.moduleGroup}:${child.moduleName}:${child.moduleVersion}"))
+         val childId = "${child.moduleGroup}:${child.moduleName}:${child.moduleVersion}"
+
+         if (!nodeMap.containsKey(childId))
          {
-            LogTools.warn("Child: " + child)
-            recursiveHandleDependency(child)
+            nodeMap[childId] = Factory.mutNode(childId)
+         }
+
+         nodeMap[id]!!.addLink(nodeMap[childId])
+
+         if (!previouslyExplored.contains(childId))
+         {
+            previouslyExplored.add(childId)
+            recursiveHandleDependency(firstLevelModuleDependency, childId)
          }
       }
    }
