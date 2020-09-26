6369d84705675c9adea617a51c890155fa2e9538
Duncan Calvert <dcalvert@ihmc.us>
2020-09-25 22:13:25 -0500

    Gradle 6 support.

--- src/junitfive-test/builds/parallel-execution/build.gradle
+++ src/junitfive-test/builds/parallel-execution/build.gradle
@@ -1,5 +1,5 @@
 plugins {
-   id("us.ihmc.ihmc-build") version "0.22.0"
+   id("us.ihmc.ihmc-build")
    id("us.ihmc.log-tools-plugin") version "0.5.0"
    id("us.ihmc.ihmc-ci") version "6.2"
    id("us.ihmc.ihmc-cd") version "1.8"
