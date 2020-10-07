4922c550fe4bc54cef8e9d26aa39f7d863986715
Duncan Calvert <dcalvert@ihmc.us>
2020-10-07 13:19:12 -0500

    Fix allocation instrumentation.

--- buildSrc/src/main/kotlin/us/ihmc/ci/IHMCCIPlugin.kt
+++ buildSrc/src/main/kotlin/us/ihmc/ci/IHMCCIPlugin.kt
@@ -361,7 +361,7 @@ class IHMCCIPlugin : Plugin<Project>
       {
          for (testProject in testProjects(project))
          {
-            testProject.configurations.getByName("runtime").files.forEach {
+            testProject.configurations.getByName("runtimeClasspath").files.forEach {
                if (it.name.contains("java-allocation-instrumenter"))
                {
                   allocationJVMArg = "-javaagent:" + it.getAbsolutePath()
