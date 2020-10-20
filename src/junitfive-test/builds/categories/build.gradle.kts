bae3bd23173f75af50f2ecab52f203ec78c731e2
Duncan Calvert <dcalvert@ihmc.us>
2020-10-20 15:17:20 -0500

    :arrow_up: ihmc-ci 6.8

--- src/junitfive-test/builds/categories/build.gradle.kts
+++ src/junitfive-test/builds/categories/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
    id("us.ihmc.ihmc-build")
    id("us.ihmc.log-tools-plugin") version "0.5.0"
-   id("us.ihmc.ihmc-ci") version "6.4"
+   id("us.ihmc.ihmc-ci") version "6.8"
    id("us.ihmc.ihmc-cd") version "1.14"
 }
 
