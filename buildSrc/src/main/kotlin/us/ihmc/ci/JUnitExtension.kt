62b995204b467f47e0d493490a7fa99e4adced70
Duncan Calvert <dcalvert@ihmc.us>
2020-10-20 11:11:32 -0500

    Get dependency addition from plugin working in Gradle 6. Work in progress.

--- buildSrc/src/main/kotlin/us/ihmc/ci/JUnitExtension.kt
+++ buildSrc/src/main/kotlin/us/ihmc/ci/JUnitExtension.kt
@@ -1,3 +1,12 @@
 package us.ihmc.ci
 
-class JUnitExtension(val jupiter: String, val platform: String)
+class JUnitExtension(val jupiterVersion: String,
+                     val platformVersion: String,
+                     val vintageVersion: String)
+{
+   fun jupiterApi(): String = "org.junit.jupiter:junit-jupiter-api:$jupiterVersion"
+   fun jupiterEngine(): String = "org.junit.jupiter:junit-jupiter-engine:$jupiterVersion"
+   fun platformCommons(): String = "org.junit.platform:junit-platform-commons:$platformVersion"
+   fun platformLauncher(): String = "org.junit.platform:junit-platform-launcher:$platformVersion"
+   fun vintage(): String = "junit:junit:$vintageVersion"
+}
