01dce8670e311b49df6fd0cb8d58a54724fabfcc
Dexton Anderson <danderson@ihmc.org>
2023-03-29 11:02:07 -0500

    Revert formatting

--- src/main/kotlin/us/ihmc/build/IHMCBuildExtension.kt
+++ src/main/kotlin/us/ihmc/build/IHMCBuildExtension.kt
@@ -1150,8 +1150,8 @@ open class IHMCBuildExtension(val project: Project)
                   classifier = ""
 
                val dependencyGAVKey = firstLevelModuleDependency.moduleGroup +
-                       ":${firstLevelModuleDependency.moduleName}" +
-                       ":${firstLevelModuleDependency.moduleVersion}"
+                                      ":${firstLevelModuleDependency.moduleName}" +
+                                      ":${firstLevelModuleDependency.moduleVersion}"
                val dependencyGAVWithClassifierKey = "$dependencyGAVKey:$classifier"
 
                if (!addedAlready.contains(dependencyGAVWithClassifierKey))
