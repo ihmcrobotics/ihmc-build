6803b8070fca773ce42dc05281b5909a38ea3387
Dexton Anderson <danderson@ihmc.org>
2024-09-23 12:56:08 -0500

    Undo removed test

---
+++ tests/generateTestSuitesTest/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+   id("us.ihmc.ihmc-build")
+   id("us.ihmc.ihmc-ci") version "7.7"
+}
+
+// TODO: Fix up to test CI stuff maybe
+
+nexusUsername = System.properties.getProperty("nexus.username")
+nexusPassword = System.properties.getProperty("nexus.password")
+
+ihmc {
+   group = "us.ihmc"
+   version = "0.1.0"
+   vcsUrl = "https://your.vcs/url"
+   openSource = true
+
+   configureDependencyResolution()
+   addPublishUrl("myVendor", "https://some.fake/my-open-vendor")
+   addPublishUrl("mySecureVendor", "https://some.fake/my-secure-vendor", "someUsername", "somePassword")
+   configurePublications()
+}
+
+mainDependencies {
+   api("org.apache.commons:commons-lang3:3.12.0")
+}
+
+testDependencies {
+}
---
+++ tests/generateTestSuitesTest/settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+   plugins {
+      id("us.ihmc.ihmc-build") version "0.29.7"
+   }
+}
+
+buildscript {
+   repositories {
+      maven { url = uri("https://plugins.gradle.org/m2/") }
+      mavenLocal()
+   }
+   dependencies {
+      classpath("us.ihmc:ihmc-build:0.29.3")
+   }
+}
+
+val ihmcSettingsConfigurator = us.ihmc.build.IHMCSettingsConfigurator(settings, logger, extra)
+ihmcSettingsConfigurator.checkRequiredPropertiesAreSet()
+ihmcSettingsConfigurator.configureExtraSourceSets()
+ihmcSettingsConfigurator.findAndIncludeCompositeBuilds()
