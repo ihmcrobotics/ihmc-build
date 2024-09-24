74e5c03656ba0f21f17ac936b400a928f58538c1
Dexton Anderson <danderson@ihmc.org>
2024-09-24 09:37:58 -0500

    Add tests for CI functions

---
+++ src/test/java/us/ihmc/ci/CategoriesTest.java
@@ -0,0 +1,52 @@
+package us.ihmc.ci;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import us.ihmc.build.GradleTestingToolsKt;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+@Tag("categories-test")
+public class CategoriesTest
+{
+   @Test
+   public void testAllTestsRun() throws IOException
+   {
+      String projectName = "categories";
+      GradleTestingToolsKt.runGradleTask("-v", projectName);
+      String cleanOutput = GradleTestingToolsKt.runGradleTask("clean", projectName);
+      Assertions.assertTrue(cleanOutput.contains("BUILD SUCCESSFUL"));
+      String output = GradleTestingToolsKt.runGradleTask("test --info -Pcategory=all", projectName);
+      Assertions.assertTrue(output.contains("BUILD FAILED"));
+
+      System.out.println("Working dir: " + Paths.get(".").toAbsolutePath());
+      String results = Files.readString(Paths.get("tests/categories/src/test/build/reports/tests/test/index.html"));
+      System.out.println(results);
+      // Asserts 11 tests pass, 1 test fails, 0 tests ignored
+      Assertions.assertTrue(results.contains(
+            "<a href=\"packages/us.ihmc.ci.html\">us.ihmc.ci</a>" + System.lineSeparator() + "</td>" + System.lineSeparator() + "<td>9</td>"
+            + System.lineSeparator() + "<td>1</td>" + System.lineSeparator() + "<td>0</td>"));
+   }
+
+   @Test
+   public void testFastTestsRun() throws IOException
+   {
+      String projectName = "categories";
+      GradleTestingToolsKt.runGradleTask("-v", projectName);
+      String cleanOutput = GradleTestingToolsKt.runGradleTask("clean", projectName);
+      Assertions.assertTrue(cleanOutput.contains("BUILD SUCCESSFUL"));
+      String output = GradleTestingToolsKt.runGradleTask("test --info -PincludeTags=fast", projectName);
+      Assertions.assertTrue(output.contains("BUILD SUCCESSFUL"));
+
+      System.out.println("Working dir: " + Paths.get(".").toAbsolutePath());
+      String results = Files.readString(Paths.get("tests/categories/src/test/build/reports/tests/test/index.html"));
+      System.out.println(results);
+      // Asserts 5 tests pass, 0 test fails, 0 tests ignored
+      Assertions.assertTrue(results.contains(
+            "<a href=\"packages/us.ihmc.ci.html\">us.ihmc.ci</a>" + System.lineSeparator() + "</td>" + System.lineSeparator() + "<td>4</td>"
+            + System.lineSeparator() + "<td>0</td>" + System.lineSeparator() + "<td>0</td>"));
+   }
+}
---
+++ tests/categories/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+   id("us.ihmc.ihmc-build")
+   id("us.ihmc.log-tools-plugin") version "0.6.3"
+   id("us.ihmc.ihmc-ci") version "7.7"
+   id("us.ihmc.ihmc-cd") version "1.23"
+}
+
+ihmc {
+   group = "us.ihmc"
+   version = "0.1.0"
+   vcsUrl = "https://your.vcs/url"
+   openSource = true
+
+   configureDependencyResolution()
+   configurePublications()
+}
+
+//categories.configure("all") {
+//
+//}
+
+//ihmc.sourceSetProject("test").tasks.named("test", Test::class.java) {
+//
+//}
+
+mainDependencies {
+   api("org.apache.commons:commons-lang3:3.12.0")
+}
+
+testDependencies {
+}
