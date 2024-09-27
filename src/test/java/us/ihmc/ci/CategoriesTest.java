df4c04158571d709fac86b06459da1123a08f7dc
Duncan Calvert <dcalvert@ihmc.org>
2024-09-27 16:42:53 -0500

    Simplify tests.

--- src/test/java/us/ihmc/ci/CategoriesTest.java
+++ src/test/java/us/ihmc/ci/CategoriesTest.java
@@ -3,57 +3,50 @@ package us.ihmc.ci;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import us.ihmc.commons.nio.FileTools;
-import us.ihmc.log.LogTools;
+import us.ihmc.build.GradleTestingToolsKt;
 
-import java.io.UnsupportedEncodingException;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Paths;
 
-/**
- * Must be run from ihmc-ci/src/junitfive-test root directory.
- */
 @Tag("categories-test")
 public class CategoriesTest
 {
    @Test
-   public void testAllTestsRun() throws UnsupportedEncodingException
+   public void testAllTestsRun() throws IOException
    {
       String projectName = "categories";
-      GradleSubBuildTools.runGradleTask("-v", projectName);
-      String cleanOutput = GradleSubBuildTools.runGradleTask("clean", projectName);
+      GradleTestingToolsKt.runGradleTask("-v", projectName);
+      String cleanOutput = GradleTestingToolsKt.runGradleTask("clean", projectName);
       Assertions.assertTrue(cleanOutput.contains("BUILD SUCCESSFUL"));
-      String output = GradleSubBuildTools.runGradleTask("test --info -Pcategory=all", projectName);
+      String output = GradleTestingToolsKt.runGradleTask("test --info -Pcategory=all", projectName);
       Assertions.assertTrue(output.contains("BUILD FAILED"));
 
-      LogTools.info("Working dir: " + Paths.get(".").toAbsolutePath());
-      String results = new String(FileTools.readAllBytes(Paths.get("builds/categories/src/test/build/reports/tests/test/index.html"), e -> Assertions.fail(e)), "UTF-8");
+      System.out.println("Working dir: " + Paths.get(".").toAbsolutePath());
+      String results = Files.readString(Paths.get("tests/categories/src/test/build/reports/tests/test/index.html"));
       System.out.println(results);
       // Asserts 11 tests pass, 1 test fails, 0 tests ignored
-      Assertions.assertTrue(results.contains("<a href=\"packages/us.ihmc.ci.html\">us.ihmc.ci</a>" + System.lineSeparator() +
-                                                   "</td>" + System.lineSeparator() +
-                                                   "<td>9</td>" + System.lineSeparator() +
-                                                   "<td>1</td>" + System.lineSeparator() +
-                                                   "<td>0</td>"));
+      Assertions.assertTrue(results.contains(
+            "<a href=\"packages/us.ihmc.ci.html\">us.ihmc.ci</a>" + System.lineSeparator() + "</td>" + System.lineSeparator() + "<td>9</td>"
+            + System.lineSeparator() + "<td>1</td>" + System.lineSeparator() + "<td>0</td>"));
    }
 
    @Test
-   public void testFastTestsRun() throws UnsupportedEncodingException
+   public void testFastTestsRun() throws IOException
    {
       String projectName = "categories";
-      GradleSubBuildTools.runGradleTask("-v", projectName);
-      String cleanOutput = GradleSubBuildTools.runGradleTask("clean", projectName);
+      GradleTestingToolsKt.runGradleTask("-v", projectName);
+      String cleanOutput = GradleTestingToolsKt.runGradleTask("clean", projectName);
       Assertions.assertTrue(cleanOutput.contains("BUILD SUCCESSFUL"));
-      String output = GradleSubBuildTools.runGradleTask("test --info -PincludeTags=fast", projectName);
+      String output = GradleTestingToolsKt.runGradleTask("test --info -PincludeTags=fast", projectName);
       Assertions.assertTrue(output.contains("BUILD SUCCESSFUL"));
 
-      LogTools.info("Working dir: " + Paths.get(".").toAbsolutePath());
-      String results = new String(FileTools.readAllBytes(Paths.get("builds/categories/src/test/build/reports/tests/test/index.html"), e -> Assertions.fail(e)), "UTF-8");
+      System.out.println("Working dir: " + Paths.get(".").toAbsolutePath());
+      String results = Files.readString(Paths.get("tests/categories/src/test/build/reports/tests/test/index.html"));
       System.out.println(results);
       // Asserts 5 tests pass, 0 test fails, 0 tests ignored
-      Assertions.assertTrue(results.contains("<a href=\"packages/us.ihmc.ci.html\">us.ihmc.ci</a>" + System.lineSeparator() +
-                                                   "</td>" + System.lineSeparator() +
-                                                   "<td>4</td>" + System.lineSeparator() +
-                                                   "<td>0</td>" + System.lineSeparator() +
-                                                   "<td>0</td>"));
+      Assertions.assertTrue(results.contains(
+            "<a href=\"packages/us.ihmc.ci.html\">us.ihmc.ci</a>" + System.lineSeparator() + "</td>" + System.lineSeparator() + "<td>4</td>"
+            + System.lineSeparator() + "<td>0</td>" + System.lineSeparator() + "<td>0</td>"));
    }
 }
--- src/test/java/us/ihmc/ci/ParallelExecutionTest.java
+++ src/test/java/us/ihmc/ci/ParallelExecutionTest.java
@@ -2,61 +2,49 @@ package us.ihmc.ci;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import us.ihmc.commons.nio.FileTools;
-import us.ihmc.log.LogTools;
+import us.ihmc.build.GradleTestingToolsKt;
 
-import java.io.UnsupportedEncodingException;
-import java.nio.charset.StandardCharsets;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Paths;
 
-/**
- * Must be run from ihmc-ci/src/junitfive-test root directory.
- */
 public class ParallelExecutionTest
 {
    @Test
-   public void testAllTestsRun() throws UnsupportedEncodingException
+   public void testAllTestsRun() throws IOException
    {
       String projectName = "categories";
-      GradleSubBuildTools.runGradleTask("-v", projectName);
-      String cleanOutput = GradleSubBuildTools.runGradleTask("clean", projectName);
+      GradleTestingToolsKt.runGradleTask("-v", projectName);
+      String cleanOutput = GradleTestingToolsKt.runGradleTask("clean", projectName);
       Assertions.assertTrue(cleanOutput.contains("BUILD SUCCESSFUL"));
-      String output = GradleSubBuildTools.runGradleTask("test --info -Pcategory=all", projectName);
+      String output = GradleTestingToolsKt.runGradleTask("test --info -Pcategory=all", projectName);
       Assertions.assertTrue(output.contains("BUILD FAILED"));
 
-      LogTools.info("Working dir: " + Paths.get(".").toAbsolutePath());
-      String results = new String(FileTools.readAllBytes(Paths.get("builds/categories/src/test/build/reports/tests/test/index.html"),
-                                                         Assertions::fail),
-                                  StandardCharsets.UTF_8);
+      System.out.println("Working dir: " + Paths.get(".").toAbsolutePath());
+      String results = new String(Files.readAllBytes(Paths.get("tests/categories/src/test/build/reports/tests/test/index.html")));
       System.out.println(results);
       // Asserts 11 tests pass, 1 test fails, 0 tests ignored
-      Assertions.assertTrue(results.contains("<a href=\"packages/us.ihmc.ci.html\">us.ihmc.ci</a>" + System.lineSeparator() +
-                                                   "</td>" + System.lineSeparator() +
-                                                   "<td>9</td>" + System.lineSeparator() +
-                                                   "<td>1</td>" + System.lineSeparator() +
-                                                   "<td>0</td>"));
+      Assertions.assertTrue(results.contains(
+            "<a href=\"packages/us.ihmc.ci.html\">us.ihmc.ci</a>" + System.lineSeparator() + "</td>" + System.lineSeparator() + "<td>9</td>"
+            + System.lineSeparator() + "<td>1</td>" + System.lineSeparator() + "<td>0</td>"));
    }
 
    @Test
-   public void testFastTestsRun() throws UnsupportedEncodingException
+   public void testFastTestsRun() throws IOException
    {
       String projectName = "categories";
-      GradleSubBuildTools.runGradleTask("-v", projectName);
-      String cleanOutput = GradleSubBuildTools.runGradleTask("clean", projectName);
+      GradleTestingToolsKt.runGradleTask("-v", projectName);
+      String cleanOutput = GradleTestingToolsKt.runGradleTask("clean", projectName);
       Assertions.assertTrue(cleanOutput.contains("BUILD SUCCESSFUL"));
-      String output = GradleSubBuildTools.runGradleTask("test --info -Pcategory=fast", projectName);
+      String output = GradleTestingToolsKt.runGradleTask("test --info -Pcategory=fast", projectName);
       Assertions.assertTrue(output.contains("BUILD SUCCESSFUL"));
 
-      LogTools.info("Working dir: " + Paths.get(".").toAbsolutePath());
-      String results = new String(FileTools.readAllBytes(Paths.get("builds/categories/src/test/build/reports/tests/test/index.html"),
-                                                         Assertions::fail),
-                                  StandardCharsets.UTF_8);
+      System.out.println("Working dir: " + Paths.get(".").toAbsolutePath());
+      String results = new String(Files.readAllBytes(Paths.get("tests/categories/src/test/build/reports/tests/test/index.html")));
       System.out.println(results);
       // Asserts 5 tests pass, 0 test fails, 0 tests ignored
-      Assertions.assertTrue(results.contains("<a href=\"packages/us.ihmc.ci.html\">us.ihmc.ci</a>" + System.lineSeparator() +
-                                                   "</td>" + System.lineSeparator() +
-                                                   "<td>4</td>" + System.lineSeparator() +
-                                                   "<td>0</td>" + System.lineSeparator() +
-                                                   "<td>0</td>"));
+      Assertions.assertTrue(results.contains(
+            "<a href=\"packages/us.ihmc.ci.html\">us.ihmc.ci</a>" + System.lineSeparator() + "</td>" + System.lineSeparator() + "<td>4</td>"
+            + System.lineSeparator() + "<td>0</td>" + System.lineSeparator() + "<td>0</td>"));
    }
 }
