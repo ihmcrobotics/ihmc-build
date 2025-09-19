5ee45c960508778d597c0da56848641acd8d9013
Duncan Calvert <dcalvert@ihmc.org>
2025-09-19 16:29:00 -0500

    Cleanup tests.

--- tests/multi-project-composite-source-sets/sub-project-two/build.gradle.kts
+++ tests/multi-project-composite-source-sets/sub-project-two/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
    id("us.ihmc.ihmc-build")
-   id("us.ihmc.ihmc-ci") version "7.7"
 }
 
 ihmc {
--- tests/parallel-execution/build.gradle.kts
+++ tests/parallel-execution/build.gradle.kts
@@ -1,8 +1,6 @@
 plugins {
    id("us.ihmc.ihmc-build")
    id("us.ihmc.log-tools-plugin") version "0.6.3"
-   id("us.ihmc.ihmc-ci") version "7.7"
-   id("us.ihmc.ihmc-cd") version "1.23"
 }
 
 ihmc {
